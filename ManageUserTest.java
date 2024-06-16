8bb62567416c048d99a0f0d7c839fa609e95be7e
khadiza21 <bibikhadiza474@gmail.com>
2024-06-16 22:27:19 +0600

    citymover website manage user by admin (verify and remove) module automated test by selenum testing tools

--- ManageUserTest.java
+++ ManageUserTest.java
@@ -1,3 +1,152 @@
+
+package seleniumtest;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.NoSuchElementException;
+
+import java.time.Duration;
+import java.util.List;
+
 public class ManageUserTest {
-    
+
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\User\\Documents\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+        String loginPageUrl = "http://localhost:5173/login";
+        String currentUrl;
+
+        String[] adminnames = {"citymover@gmail.com"};
+        String[] passwords = {"@192Bk"};
+
+        boolean loginSuccessful = false;
+        for (int i = 0; i < adminnames.length; i++) {
+            driver.get(loginPageUrl);
+
+            WebElement emailField = driver.findElement(By.className("email"));
+            emailField.clear();
+            emailField.sendKeys(adminnames[i]);
+
+            WebElement passField = driver.findElement(By.className("password"));
+            passField.clear();
+            passField.sendKeys(passwords[i]);
+
+            Thread.sleep(5000);
+
+            WebElement loginButton = driver.findElement(By.id("loginButton"));
+            loginButton.click();
+
+            Thread.sleep(5000);
+
+            currentUrl = driver.getCurrentUrl();
+            if (currentUrl.equals("http://localhost:5173/")) {
+                System.out.println("ok");
+                loginSuccessful = true;
+                break;
+            } else {
+                System.out.println("no ok");
+            }
+        }
+
+        if (loginSuccessful) {
+
+            driver.get("http://localhost:5173/dashboard");
+            Thread.sleep(2500);
+
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+            WebElement profileIcon = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("profileIcon")));
+            highlightElement(driver, profileIcon);  
+            profileIcon.click();
+            System.out.println("Clicked profile icon");
+            Thread.sleep(2000);
+
+            WebElement userManagementLink = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("userManagement")));
+            highlightElement(driver, userManagementLink);  // Highlight the user management link
+            Thread.sleep(5000);
+            userManagementLink.click();
+            System.out.println("Clicked user management link");
+
+            Thread.sleep(2500);
+
+            currentUrl = driver.getCurrentUrl();
+            if (currentUrl.equals("http://localhost:5173/manageuser")) {
+                System.out.println("Navigated to user management successfully.");
+                Thread.sleep(2000);
+                List<WebElement> users = driver.findElements(By.className("userItem"));
+
+                int verifiedCount = 0;
+                int removedCount = 0;
+
+                // Skip the first 4 users
+                for (int i = 4; i < users.size(); i++) {
+                    WebElement user = users.get(i);
+                    boolean isOdd = (i % 2 == 1);
+
+                    try {
+                        WebElement verifyButton = user.findElement(By.className("verifyButton"));
+
+                        if (isOdd && verifiedCount < 2) {
+                            // Odd iteration: Verify the user
+                            highlightElement(driver, verifyButton);  // Highlight the verify button
+                            Thread.sleep(5000);
+                            verifyButton.click();
+                            
+                            System.out.println("Clicked verify button for user " + (i + 1));
+                            Thread.sleep(1500);
+                            System.out.println("Verified user " + (i + 1));
+                            verifiedCount++;
+                        } else if (!isOdd && removedCount < 2) {
+                            // Even iteration: Skip to next user
+                            System.out.println("Skipping user " + (i + 1) + " for deletion as verification button is present.");
+                        } else {
+                            System.out.println("Skipping user " + (i + 1));
+                        }
+
+                    } catch (NoSuchElementException e) {
+                        if (!isOdd && removedCount < 2) {
+                            WebElement removeButton = user.findElement(By.className("removeButton"));
+                            highlightElement(driver, removeButton);  // Highlight the remove button
+                            
+                            removeButton.click();
+                            System.out.println("Clicked remove button for user " + (i + 1));
+                            Thread.sleep(1500);
+                            System.out.println("Removed user " + (i + 1));
+                            removedCount++;
+
+                            // Confirm the removal if necessary
+                            WebElement confirmButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[text()='Yes, delete it!']")));
+                            highlightElement(driver, confirmButton);  // Highlight the confirm button
+                            confirmButton.click();
+                            System.out.println("Clicked confirm button for removing user " + (i + 1));
+                            Thread.sleep(1500);
+                        } else {
+                            System.out.println("Skipping user " + (i + 1) + " for verification as verify button is not present.");
+                        }
+                    }
+
+                    if (verifiedCount >= 2 && removedCount >= 2) {
+                        break;
+                    }
+                }
+            } else {
+                System.out.println("Failed to navigate to user management.");
+            }
+        } else {
+            System.out.println("All login attempts failed.");
+        }
+
+        driver.quit();
+    }
+
+    private static void highlightElement(WebDriver driver, WebElement element) {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("arguments[0].style.border='3px solid red'", element);
+    }
 }
--- RIdeConfiremCancel.java
+++ RIdeConfiremCancel.java
@@ -8,74 +8,7 @@ import java.util.Random;
 
 public class RIdeConfiremCancel {
 
-    public static void main(String[] args) throws InterruptedException {
+ 
 
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\User\\Documents\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-
-        driver.manage().window().maximize();
-        boolean loginSuccessful = false;
-
-        while (!loginSuccessful) {
-            String signupPageUrl = "http://localhost:5173/signup";
-            driver.get(signupPageUrl);
-
-            String[] roles = {"User"};
-            String[] genders = {"Female", "Male", "Other"};
-            String[] names = {"name one", "name two", "name three", "name four", "name five", "name six"};
-
-            Random random = new Random();
-            String selectedRole = roles[random.nextInt(roles.length)];
-
-            Select roleSelect = new Select(driver.findElement(By.className("role")));
-            roleSelect.selectByVisibleText(selectedRole);
-
-            String selectedName = names[random.nextInt(names.length)];
-            WebElement nameField = driver.findElement(By.className("name"));
-            nameField.sendKeys(selectedName);
-
-            String cleanName = selectedName.replaceAll("\\s+", ""); 
-            String randomEmail = cleanName + random.nextInt(1000) + "@gmail.com";
-            WebElement emailField = driver.findElement(By.className("email"));
-            emailField.sendKeys(randomEmail);
-
-            String phoneNumber = "1" + (random.nextInt(90000000) + 10000000);
-            if (random.nextBoolean()) {
-                phoneNumber += random.nextInt(10);
-            }
-            WebElement phoneField = driver.findElement(By.className("phone"));
-            phoneField.sendKeys(phoneNumber);
-
-            String selectedGender = genders[random.nextInt(genders.length)];
-            Select genderSelect = new Select(driver.findElement(By.className("gender")));
-            genderSelect.selectByVisibleText(selectedGender);
-
-            String password = "@192Bk";
-
-            WebElement passField = driver.findElement(By.className("password"));
-            passField.sendKeys(password);
-            WebElement confirmPassField = driver.findElement(By.className("confirmPassword"));
-            confirmPassField.sendKeys(password);
-
-            Thread.sleep(5000);
-
-            WebElement submitButton = driver.findElement(By.id("submit"));
-            submitButton.click();
-
-            Thread.sleep(10000);
-
-            String currentUrl = driver.getCurrentUrl();
-            if (currentUrl.equals("http://localhost:5173/")) {
-                System.out.println("Successfully Created Account.");
-                loginSuccessful = true;
-                break;
-            } else {
-                System.out.println("Provide right Information. And Try again");
-            }
-
-        }
-
-        driver.quit();
-    }
 }
 
